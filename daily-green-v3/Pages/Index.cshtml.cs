3d85734a5a2fb95571b040ca2773029239040171
Angel Meza <undead.85@gmail.com>
2019-05-04 21:42:44 -0700

    vs project

---
+++ daily-green-v3/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace daily_green_v3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ daily-green-v3/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var score = 0;
+
+var data = {data }
+
+    $(document).ready(function () {
+        $("#option1").click(function () {
+            score += 10;
+            //alert(score);
+            $("#score").html(score);
+
+            $("#question").html("Question2");
+            
+        });
+    });
